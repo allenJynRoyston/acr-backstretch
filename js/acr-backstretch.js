f31f6ecfab35bfdd9c237cef7b50071eab819d35
Allen Royston <allen.acr@gmail.com>
2016-04-04 11:12:57 -0700

    Update acr-backstretch.js

--- js/acr-backstretch.js
+++ js/acr-backstretch.js
@@ -11,10 +11,15 @@ angular.module('acrBackstretch', []).directive('backstretch', [
           fade: '='
         },
         link: function(scope, elem, attr) {
-              $(elem).css({'width': 'auto',  'height': scope.fillheight});
-              if(scope.duration == undefined){scope.duration = 500;};
-              if(scope.fade == undefined){scope.fade = 500;};
-              $(elem).backstretch([scope.fillsrc], {duration: scope.duration, fade: scope.fade});
+
+              var t = {
+                duration: scope.duration || 500,
+                fade: scope.fade || 500,
+                fillheight: scope.fillheight || 250
+              }
+
+              $(elem).css({'width': 'auto',  'height': t.fillheight});
+              $(elem).backstretch([scope.fillsrc], {duration: t.duration, fade: t.fade});
         },
 
     };
